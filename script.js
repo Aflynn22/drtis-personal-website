d0c516e127e46ded7c4fb608e692ef9d4256a9cc
Austin <Austin.flynn1992@gmail.com>
2023-09-08 13:11:18 -0500

    reverted alot of the images and styling, they would not appear on the live version

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Ill get better lol I want to make you a Binx website with her in allof her moods!";
+h2.textContent = "This text was added with script!";
 document.querySelector("body").appendChild(h2);
 
