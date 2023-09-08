cf9e0bf3632771766906460e24011b7129de5ee5
Austin <Austin.flynn1992@gmail.com>
2023-09-08 15:56:06 -0500

    made changes to backround and photos listed and added

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This text was added with script!";
+h2.textContent = "I will have some buttons to click and pictures to pop up with captions soon!";
 document.querySelector("body").appendChild(h2);
