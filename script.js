6619ba766e5b2ecc8858e60a5865903afd781abc
Austin <Austin.flynn1992@gmail.com>
2023-09-08 13:02:08 -0500

    I made changes to index.html, changed style.cssand also the script.js, added images changed text and colors

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Ill get better lol I want to make you a Binx website with her in allof her moods!";
 document.querySelector("body").appendChild(h2);
+
